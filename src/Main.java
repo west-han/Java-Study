163c632295773d856d4017fd9a56d01a7e7a390c
SeokHyeon Han <han@westhan.local>
2025-01-22 22:29:33 +0900

    2025-01-22

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        /*
+        // 클래스 생성
+        MyFirstClass myFirstClass = new MyFirstClass();
+        System.out.println(myFirstClass.getClass());
+        System.out.println(myFirstClass.getData());
+        myFirstClass.setData(100);
+        System.out.println(myFirstClass.getData());
+         */
+
+
+    }
+}
---
+++ src/MyFirstClass.java
@@ -0,0 +1,15 @@
+public class MyFirstClass {
+    private int data;
+
+    public MyFirstClass() {
+        System.out.println("MyFirstClass.MyFirstClass - constructor called");
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int param) {
+        this.data = param;
+    }
+}

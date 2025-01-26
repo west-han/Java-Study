619ee9bf220fe89e0c72412468f83d87cdf6f135
west-han <westhan98@gmail.com>
2025-01-26 23:44:27 +0900

    20250126

---
+++ src/Enum.java
@@ -0,0 +1,12 @@
+enum Week {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
+
+public class Enum {
+    public static void main(String[] args) {
+        Week week = Week.SUNDAY;
+        System.out.println(Week.valueOf("MONDAY").ordinal()); // 순서
+        System.out.println(week.name());
+        System.out.println(week.ordinal());
+    }
+}
--- src/Polymorphism.java
+++ src/Polymorphism.java
@@ -1,3 +1,66 @@
-public class Polymorphism {
+import java.util.Scanner;
 
+class Shape {
+    private final int size;
+
+    public Shape(int size) {
+        this.size = size;
+    }
+
+    public void render() {
+        System.out.println();
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
+
+class Triangle extends Shape {
+    public Triangle(int size) {
+        super(size);
+    }
+
+    @Override
+    public void render() {
+        for (int i = 0; i < super.getSize(); i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Rectangle extends Shape {
+    public Rectangle(int size) {
+        super(size);
+    }
+
+    @Override
+    public void render() {
+        for (int i = 0; i < super.getSize(); i++) {
+            for (int j = 0; j < super.getSize(); j++) {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+    }
+}
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        Shape s = null;
+
+        if (input == 0) {
+            s = new Triangle(5);
+        } else {
+            s = new Rectangle(5);
+        }
+
+        s.render();
+        System.out.println(s instanceof Triangle);
+    }
 }

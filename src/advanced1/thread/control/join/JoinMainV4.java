326eb6970cebfb59e20c62e1759365122ec6b60c
west-han <westhan98@gmail.com>
2025-03-07 23:54:52 +0900

    20250307

---
+++ src/advanced1/thread/control/join/JoinMainV4.java
@@ -0,0 +1,50 @@
+package advanced1.thread.control.join;
+
+import advanced1.util.ThreadUtils;
+
+import static advanced1.util.MyLogger.log;
+
+public class JoinMainV4 {
+    public static void main(String[] args) throws InterruptedException {
+        log("Start");
+
+        SumTask task1 = new SumTask(1, 50);
+        Thread thread1 = new Thread(task1, "thread-1");
+
+        thread1.start();
+
+        // 스레드가 종료될 때까지 대기
+        log("join() - main 스레드가 thread1 종료까지 1초 대기");
+        thread1.join(1000);
+        log("main 스레드 대기 완료");
+        log("task1.result = " + task1.result);
+
+        log("End");
+    }
+  
+    static class SumTask implements Runnable {
+
+        int startValue;
+        int endValue;
+        int result;
+
+        public SumTask(int startValue, int endValue) {
+            this.startValue = startValue;
+            this.endValue = endValue;
+        }
+
+
+        @Override
+        public void run() {
+            log("작업 시작");
+            ThreadUtils.sleep(2000);
+            int sum = 0;
+            for (int i = startValue; i <= endValue; i++) {
+                sum += i;
+            }
+            result = sum;
+            log("작업 완료 result=" + result);
+        }
+
+    }
+}
